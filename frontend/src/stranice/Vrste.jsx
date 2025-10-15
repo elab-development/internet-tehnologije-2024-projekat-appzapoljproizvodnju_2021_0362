51b41afbbacfd5b756d700aee47a4c3ef4779423
Skuan <jovanstojkovv@gmail.com>
2025-10-15 23:33:28 +0200

    dodate vrste na stranicu vrste, sredjen footer i header deo

--- frontend/src/stranice/Vrste.jsx
+++ frontend/src/stranice/Vrste.jsx
@@ -1,9 +1,14 @@
-
+import podaci from "../podaci/vrste.json";
+import Kartica from "../komponente/Kartica";
 
 function Vrste() {
   return (
     <>
-
+      <div className="sve-kartice">
+        {podaci.map((kartica, index) => (
+          <Kartica key={kartica.id} index={index} slika={kartica.slika} naslov={kartica.naslov} tekst={kartica.tekst} />
+        ))}
+      </div>
     </>
   );
 }

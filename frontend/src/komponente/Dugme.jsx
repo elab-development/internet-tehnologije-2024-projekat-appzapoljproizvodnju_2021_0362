c4bc04e38e648f5038a99c337e9731099c3caedb
Skuan <jovanstojkovv@gmail.com>
2025-10-16 20:48:05 +0200

    ubacena komponenta dugme

--- frontend/src/komponente/Dugme.jsx
+++ frontend/src/komponente/Dugme.jsx
@@ -1,1 +1,11 @@
+import { Link } from "react-router-dom";
 
+function Dugme({ tekst, link }) {
+  return (
+    <Link to={link} className="dugme-link">
+      {tekst}
+    </Link>
+  );
+}
+
+export default Dugme
